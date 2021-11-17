1a0a5ba04198cc9f0f1b0621cdb3aacde1a481a9
Pete L <77577958+Beat78@users.noreply.github.com>
2021-11-17 13:27:42 +0200

    new database
    new database

--- Kalorilaskuri/kalorilaskuri/settings.py
+++ Kalorilaskuri/kalorilaskuri/settings.py
@@ -75,14 +75,15 @@ WSGI_APPLICATION = 'kalorilaskuri.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'USER': '',
-        #'PASSWORD': '',
-        #'NAME': ''
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'HOST': 'localhost',
+         'USER': 'postgres',
+         'PASSWORD': 'Q2werty',
+         'NAME': 'ruoka_aineet',
+         'PORT': '5432'
+     }
+ }
 
 
 # Password validation
